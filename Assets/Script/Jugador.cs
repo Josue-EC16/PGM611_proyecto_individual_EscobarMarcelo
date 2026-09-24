c941e88a0507ba83adc4d4661f5901ea9bc738ce
Marcelo Escobar <242859000+Josue-EC16@users.noreply.github.com>
2026-09-24 16:40:16 -0400

    SEXTO PASO: Condiguración del salto del personaje

--- Assets/Script/Jugador.cs
+++ Assets/Script/Jugador.cs
@@ -5,6 +5,11 @@ public class Jugador : MonoBehaviour
     public float velocidad = 5f;
     private Rigidbody2D rb;
     private float movimiento;
+    public float alturaSalto = 4f;
+    private bool esPiso;//true=estamos en el piso, false=estamos en el aire
+    public Transform comprobadorPiso;
+    public float radioComprobadorPiso = 0.1f;
+    public LayerMask layerPiso;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -14,5 +19,13 @@ public class Jugador : MonoBehaviour
         movimiento = Input.GetAxisRaw("Horizontal");
         rb.linearVelocity = new Vector2(movimiento * velocidad, rb.linearVelocity.y);
         if (movimiento!=0) transform.localScale = new Vector3(Mathf.Sign(movimiento),1,1);
+        if(Input.GetButtonDown("Jump")&&esPiso)
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, alturaSalto);
+    }
+
+    public void FixedUpdate()
+    {
+        esPiso = Physics2D.OverlapCircle(comprobadorPiso.position, radioComprobadorPiso, layerPiso);
     }
 }
+
